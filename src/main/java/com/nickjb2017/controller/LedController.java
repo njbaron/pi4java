f9a3753a19b356f57c887b8da3b8135b1e054449
Nicholas Baron <njbaron@users.noreply.github.com>
2017-12-29 17:01:22 -0700

    init struff

---
+++ src/main/java/com/nickjb2017/controller/LedController.java
@@ -0,0 +1,26 @@
+package com.nickjb2017.controller;
+
+import com.pi4j.io.gpio.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+public class LedController {
+
+    private GpioPinDigitalOutput pin;
+
+    @RequestMapping("/")
+    public String greeting() {
+        return "Hello World!";
+    }
+
+        @RequestMapping("/light")
+    public String light() {
+        if (pin == null) {
+            GpioController gpio = GpioFactory.getInstance();
+            pin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_04, "MyLED", PinState.LOW);
+        }
+
+        pin.toggle();
+        return "OK";
+    }
+
+}
---
+++ src/main/java/com/nickjb2017/pi4java/Application.java
@@ -0,0 +1,15 @@
+package com.nickjb2017.pi4java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(Application.class, args);
+
+    }
+
+}
